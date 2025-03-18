c11e1f0855b36c8e30d5840c2e55f203d2dd908a
yuka <youxiangyuanye@gmail.com>
2025-03-18 07:16:42 +0000

    toppage

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Alice\MovieController;
+Route::controller(MovieController::class)->group(function() {
+    Route::get('alice/movie/create', 'add');
+});
