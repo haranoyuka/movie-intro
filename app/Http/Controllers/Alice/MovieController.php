fff699a0de4b1ea42982172908b7eee36742a3b7
yuka <youxiangyuanye@gmail.com>
2025-04-04 10:34:58 +0000

    detabase

--- app/Http/Controllers/Alice/MovieController.php
+++ app/Http/Controllers/Alice/MovieController.php
@@ -14,7 +14,28 @@ class MovieController extends Controller
     }
     public function create(Request $request)
     {
+        $this->validate($request, Movie::$rules);
         return redirect('alice/movie/create');
+
+        $movie = new Movie;
+        $form = $request->all();
+
+        
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $movie->image_path = null;
+        }
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $movie->fill($form);
+        $movie->save();
     }
     
     public function academy()
